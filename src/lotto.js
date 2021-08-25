0c51d9223a9cd2cbe3e5be47bcc41703509978f4
JongHan Lee <62053466+ejonghan@users.noreply.github.com>
2021-08-25 18:48:00 +0900

    lotto number API complete

--- src/lotto.js
+++ src/lotto.js
@@ -2,13 +2,27 @@ function response(room, msg, sender, isGroup, replier) {
   var data = Utils.getWebText(
     "https://m.search.naver.com/search.naver?query=로또"
   );
-  data = data.replace(/<[^>]+>/g, "");
-  data = data.split("로또")[1];
-  data = data.split("QR")[0];
+  data = data.replace(/<[^>]+>/g, ""); //태그 전부 삭제
+  data = data.split("지급기한")[1]; //로또 정보 앞부분 삭제
+  data = data.split("QR")[0]; //로또 정보 뒷부분 삭제
   data = data.trim();
   data = data.split("\n");
 
   var lotto_Num = [];
+
+  lotto_Num[0] = data[6].trim();
+  lotto_Num[1] = data[7].trim();
+  lotto_Num[2] = data[8].trim();
+  lotto_Num[3] = data[9].trim();
+  lotto_Num[4] = data[10].trim();
+  lotto_Num[5] = data[11].trim();
+  lotto_Num[6] = data[12].trim() + data[13].trim();
+
+  var result =
+    "[이번주 로또 번호]\n\n" +
+    "☆☆☆☆☆☆☆☆☆☆☆☆☆\n" +
+    lotto_Num.join("  ") +
+    "\n☆☆☆☆☆☆☆☆☆☆☆☆☆";
 
   replier.reply(result);
 }
